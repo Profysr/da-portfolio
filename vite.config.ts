0987ab351b26cfc979a8b5b004cfc31c9be1c918
Bilal Ahmad <m.profysrbilal.pk76@gmail.com>
2026-08-15 17:43:31 +0500

    feat: Initialized Vite Project with Typescript
    - Update the settings to allow jsx, js in the project
    - Install and setup tailwind v4 with dependencies clsx, tailwind merge, tailwind-animate-css.
    - Implemented utility function for class name merging.
    - Set up main entry point for React application.
    - Configured TypeScript with appropriate settings for app and node environments.
    - Created Paths and Update Vite configuration with React and Tailwind CSS plugins.

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
